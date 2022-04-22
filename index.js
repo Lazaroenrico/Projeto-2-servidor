e291d2673fe1baf4a264efa2fd55178fc1d951dc
Lázaro Enrico <lazaro.enr.menezes@gmail.com>
2022-04-22 14:50:21 -0300

    Correção do index

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-const connectToDatabase = require("./src/database/database.js");
 const express = require("express");
 const cors = require("cors");
 const routes = require("./src/routes/anime.routes.js");
+const connectToDatabase = require("./src/database/database.js");
 
 const port = 3000;
 const app = express();
 
+connectToDatabase();
+
 app.use(express.json());
 app.use(cors());
-app.use("/anime", routes);
+app.use("/animes", routes);
 
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}  `);
